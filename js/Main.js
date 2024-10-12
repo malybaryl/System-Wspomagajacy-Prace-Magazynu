33913d4f2d6fb6b8259f9bd2b44f608ea10db51e
malybaryl <kacper_barylowicz@wp.pl>
2024-10-12 15:09:45 +0200

    js: klasa Tile + Generowanie Tiles, klasa Main
    Zmiany -Dodano klasę Tile, w ktorej jest mozliwosc wygenerowania calego tila z przedmiotem - Usunieto spora czesc kodu html -Funkcja main zostala napisana obiktowo i jest klasa Main

---
+++ js/Main.js
@@ -0,0 +1,18 @@
+import Tile from "./Tile.js";
+
+class Main {
+  #tilesArray = [];
+
+  constructor() {
+    this.generateTiles(20);
+  }
+
+  generateTiles(amountOfImages) {
+    for (let i = 0; i < amountOfImages; i++) {
+      const tile = new Tile();
+      this.#tilesArray.push(tile);
+    }
+  }
+}
+
+const main = new Main();

4e1e6b1ee5f7da83d9e96727629dddedd43ffa34
SzS19 <szymonstach03@gmail.com>
2024-10-15 20:24:21 +0200

    Backend + HTML + CSS

---
+++ Backend + HTML/magazyn/src/main/java/org/example/magazyn/UserController.java
@@ -0,0 +1,53 @@
+package org.example.magazyn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+public class UserController {
+
+    private final UserRepo userRepo;
+
+    @Autowired
+    public UserController(UserRepo userRepo) {
+        this.userRepo = userRepo;
+    }
+
+    @GetMapping("/")
+    public String index(Model model) {
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(Model model) {
+        return "login";
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestParam String email, @RequestParam String password, Model model) {
+        User user = userRepo.findByEmail(email);
+        if (user != null && user.getPassword().equals(password)) {
+            return "index";
+        } else {
+            model.addAttribute("error", "Zły email albo hasło");
+            return "login";
+        }
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+
+    @PostMapping("/register")
+    public String registerUser(@ModelAttribute User user, Model model) {
+        if (userRepo.findByEmail(user.getEmail()) != null) {
+            model.addAttribute("error", "E-mail już istnieje.");
+            return "register";
+        }
+        userRepo.save(user);
+        return "index";
+    }
+}
---
+++ Backend + HTML/magazyn/src/test/java/org/example/magazyn/MagazynApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.magazyn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagazynApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

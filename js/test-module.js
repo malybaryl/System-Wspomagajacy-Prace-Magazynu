47b6fa11802a06a6ff9b6f1fc558d05399442c0a
Piotr Samborski <ps302746@student.polsl.pl>
2024-11-12 19:54:39 +0100

    Dodanie logo

--- js/test-module.js
+++ js/test-module.js
@@ -7,6 +7,8 @@
     pliku .js
 */
 
+
+
 export class HelloWorld {
   // zmienna prywatna #zmienna
   #date = Date();
@@ -15,13 +17,13 @@ export class HelloWorld {
     // publiczna zmienna
     this._text = text;
   }
-
   // przykładowa publiczna funkcja
   PrintData = function () {
     // console.log() wyświetlanie danych w konsoli
     // otwarcie kosoli przeglądarka -> F12 -> console
     console.log(this._text + " | Date: " + this.#date);
   };
+  
 }
 
 // dziedziczenie

4045cdb7b130616d4867ee72036b9dd16d076703
SzS19 <szymonstach03@gmail.com>
2024-11-19 09:19:27 +0100

    Backend role bez html i css

---
+++ Backend UPDATE/src/main/java/org/example/magazyn/repository/RoleRepository.java
@@ -0,0 +1,10 @@
+package org.example.magazyn.repository;
+
+import org.example.magazyn.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
